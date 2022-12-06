3f5346bb919de5a99004ac6aefd71455eb267fd4
griogrii_riabov <garyabov@mts.ru>
2022-12-06 18:54:31 +0400

    some txt

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,35 +1,75 @@
 package com.example.myapplication;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TEST";
+    public static final String SOME_VALUE = "Some value";
+    private static final int RESULT_SPEECH = 10;
+    private TextView  textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: OnCreate");
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
         Button button = findViewById(R.id.button);
-        button.setOnLongClickListener(getLong_click());
+        button.setOnLongClickListener(getLongClick());
         button.setOnClickListener(v -> {
-//            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//            startActivity(intent);
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra(SOME_VALUE, "some txt");
+            startActivityForResult(intent, 1);
         });
 
-
-
+        Button recButton = findViewById(R.id.button3);
+        recButton.setOnClickListener(l -> {
+            Intent intent = new Intent(
+                    RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    "ru-RU");
+            try {
+                startActivityForResult(intent, RESULT_SPEECH);
+            } catch (ActivityNotFoundException a) {
+                Toast.makeText(getApplicationContext(),
+                        "текст не распознан",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private View.OnLongClickListener getLong_click() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1: {
+                Toast.makeText(this, data != null ? data.getStringExtra(SecondActivity.NEW_EXTRA) : "Empty", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textView.setText(text.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+    private View.OnLongClickListener getLongClick() {
         return new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -37,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         };
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: onRestart");
     }
 
     @Override
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,24 +1,50 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import static com.example.myapplication.MainActivity.SOME_VALUE;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
     private static final String TAG = "TEST2";
+    public static final String NEW_EXTRA = "NEW_EXTRA";
+
+    private String editableTxt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: OnCreate");
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String string = intent.getStringExtra(SOME_VALUE);
+        EditText textView = findViewById(R.id.textView);
+        textView.setText(string);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(l -> {
+            editableTxt = textView.getText().toString();
+            Intent intent1 = new Intent();
+            intent1.putExtra(NEW_EXTRA, editableTxt == null ? "No input txt" : editableTxt);
+            setResult(RESULT_OK, intent1);
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart: onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: onRestart");
     }
 
     @Override
